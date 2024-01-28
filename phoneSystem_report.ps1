ebce38e37fa46305e622808da37e14ad6d7dab42
Yas <129212589+Yaxmen@users.noreply.github.com>
2024-01-28 09:50:58 -0300

    Update phoneSystem_report.ps1

--- phoneSystem_report.ps1
+++ phoneSystem_report.ps1
@@ -1,4 +1,4 @@
-﻿$username = "samig01@petrobras.com.br"
+﻿$username = "@.com.br"
 $msolKeyAuth = (3,4,2,3,56,34,254,222,1,1,2,23,42,54,33,233,1,34,2,7,6,5,35,43)
 $password = Get-Content "D:\Util\PhoneSystem\_pass.sec" -ErrorAction Stop | ConvertTo-SecureString -Key $msolKeyAuth -ErrorAction Stop
 $credential = New-Object -typename System.Management.Automation.PSCredential -argumentlist $username,$password -ErrorAction Stop
